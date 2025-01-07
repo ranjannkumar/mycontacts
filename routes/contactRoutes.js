1d3cd774b3f70e9e765e5c2c688f473f08a08c34
Ranjan kumar <ranjankumar5673@gmail.com>
2025-01-07 14:48:43 +0530

    routes created

---
+++ routes/contactRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+router.route("/").get((req,res)=>{
+  res.status(200).json({message:"Get all contacts"});
+});
+
+router.route("/").post((req,res)=>{
+  res.status(200).json({message:"create conatact"});
+});
+
+router.route("/:id").get((req,res)=>{
+  res.status(200).json({message:`Get contact for ${req.params.id}`});
+});
+
+router.route("/:id").put((req,res)=>{
+  res.status(200).json({message:`Update contact for ${req.params.id}`});
+});
+
+router.route("/:id").delete((req,res)=>{
+  res.status(200).json({message:`delete contact for ${req.params.id}`});
+});
+
+module.exports=router
--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("I am in express project");
+const express=require("express");
+const dotenv = require("dotenv").config();
+
+const app=express();
+
+const port=process.env.PORT || 5000;
+
+app.use("/api/contacts",require("./routes/contactRoutes"))
+
+app.listen(port,()=>{
+  console.log(`Server running on port ${port}`);
+  
+})
