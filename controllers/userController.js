6f530952d6ba8b490476a1c2bff33843093ea548
Ranjan kumar <ranjankumar5673@gmail.com>
2025-01-08 23:09:06 +0530

    feat: authentication with jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,11 +37,33 @@ const registerUser=asyncHandler(async(req,res)=>{
 })
 
 const loginUser=asyncHandler(async(req,res)=>{
-  res.json({message:"login user"});
+  const {email,password}=req.body;
+  if(!email || !password){
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const user = await User.findOne({email});
+  if(user && (await bcrypt.compare(password,user.password))){
+    const accessToken = jwt.sign({
+      user:{
+        username:user.username,
+        email:user.email,
+        id:user.id,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn: "1m"}
+  )
+   res.status(200).json({accessToken});
+  }else{
+    res.status(401);
+    throw new Error("email or password is not valid");
+    
+  }
 })
 
 const currentUser=asyncHandler(async(req,res)=>{
-  res.json({message:"current user information"});
+  res.json(req.user);
 })
 
 module.exports={
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { registerUser, loginUser, currentUser } = require("../controllers/userController");
+const validateToken = require("../middleware/validateTokenHandler");
 const userRouter = express.Router();
 
 userRouter.post("/register",registerUser)
 
 userRouter.post("/login",loginUser)
-userRouter.get("/current",currentUser)
+userRouter.get("/current",validateToken, currentUser)
 
 module.exports=userRouter;
